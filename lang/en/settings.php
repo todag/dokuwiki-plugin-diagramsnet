241fdfe49a7c585ea922b18f37bf5e886c4bf84b
todag <33574714+todag@users.noreply.github.com>
2020-08-12 21:30:03 +0000

    Remove remnants of old name (draw.io) from settings.

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['app_source_type'] = 'Use the included draw.io package (internal) or use the online version from external_url (external)';
-$lang['editor_parameters'] = 'Parameters for draw.io editor';
-$lang['viewer_parameters'] = 'Parameters for draw.io viewer';
-$lang['external_url'] = 'URL to external draw.io application';
+$lang['app_source_type'] = 'Use the included diagrams.net package (internal) or use the online version from external_url (external)';
+$lang['editor_parameters'] = 'Parameters for diagrams.net editor';
+$lang['viewer_parameters'] = 'Parameters for diagrams.net viewer';
+$lang['external_url'] = 'URL to external diagrams.net application';
 $lang['file_match_suffix'] = 'The plugin will grab and render any file containing this in its name';
 $lang['enable_attic'] = 'Enable file revisions for diagram files';
